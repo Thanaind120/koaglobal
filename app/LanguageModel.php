e0fe89454a19da103d5337189c1a0d40e19edca9
Thanai Likitpoonsup <thanai.ots2565@gmail.com>
2022-02-10 12:14:24 +0700

    update

--- app/LanguageModel.php
+++ app/LanguageModel.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class LanguageModel extends Model
 {
     protected $table = 'tb_language';
-    protected $fillable = ['id','name','sort','status','created','updated'];
+    protected $fillable = ['id','name','class','set','sort','status','created','updated'];
     const CREATED_AT = 'created';
     const UPDATED_AT = 'updated';
     public $timestamp = false;
